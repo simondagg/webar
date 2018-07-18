f9bdb3e9444df2f3b584376760f53788c3760b7b
simondagg <31724076+simondagg@users.noreply.github.com>
2018-07-19 02:02:25 +0800

    Update han.js

--- demo2/js/han.js
+++ demo2/js/han.js
@@ -26,4 +26,7 @@ function getgps() {
     }
     navigator.geolocation.getCurrentPosition(success, error)
 }
+document.querySelector('a-box').addEventListener('collide', function (evt) {
+    console.log('This A-Frame entity collided with another entity!');
+  });
 setInterval(getgps, 5000);
