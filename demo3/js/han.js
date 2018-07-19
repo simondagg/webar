650b59bd159a6a27890d098c575cbaaa21aaba46
simondagg <31724076+simondagg@users.noreply.github.com>
2018-07-19 16:00:34 +0800

    Add files via upload

---
+++ demo3/js/han.js
@@ -0,0 +1,31 @@
+var front = false;
+
+var constraints = { video: { facingMode: (front ? "user" : "environment"), width: 1280, height: 720 } };
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (mediaStream) {
+        var video = document.querySelector('video');
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = function (e) {
+            video.play();
+        };
+    })
+    .catch(function (err) { console.log(err.name + ": " + err.message); });
+function getgps() {
+
+    function error() {
+        alert("Unable to retrieve your location");
+    }
+
+
+    function success(position) {
+        var latitude = position.coords.latitude * 100000;
+        var longitude = position.coords.longitude * 100000;
+        var coordinate = latitude.toString() + " 0 " + longitude.toString();
+        document.getElementById('camera').setAttribute('position', coordinate);
+        var boxcoordinate = (latitude+1).toString() + " 0 " + (longitude+1).toString();
+        document.getElementById('box').setAttribute('position', boxcoordinate);
+
+    }
+    navigator.geolocation.getCurrentPosition(success, error)
+}
+setInterval(getgps, 1000);
