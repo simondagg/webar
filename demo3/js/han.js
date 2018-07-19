f0b73c1e9dd9649c0fd6a9a14c54a3e4eaa3eeea
simondagg <31724076+simondagg@users.noreply.github.com>
2018-07-19 21:35:36 +0800

    Update han.js

--- demo3/js/han.js
+++ demo3/js/han.js
@@ -24,7 +24,7 @@ function getgps() {
       
         var camera= document.getElementById('camera');
         
-         camera..object3D.position.set(latitude,0,longitude)
+         camera.object3D.position.set(latitude,0,longitude);
         document.getElementById('box').object3D.position.set(camera.object3D.position.x+5,0, camera.object3D.position.z+5);
 
     }
