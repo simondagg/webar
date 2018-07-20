4b85a65eda7ac71d499ba6a3da4b991824999330
simondagg <31724076+simondagg@users.noreply.github.com>
2018-07-20 11:55:24 +0800

    Update han.js

--- demo_gpsless/js/han.js
+++ demo_gpsless/js/han.js
@@ -1,6 +1,6 @@
 var front = false;
 
-var constraints = { video: { facingMode: (front ? "user" : "environment"), width: 1280, height: 720 } };
+var constraints = { video: { facingMode: (front ? "user" : "environment"), width: 800, height: 450 } };
 navigator.mediaDevices.getUserMedia(constraints)
     .then(function (mediaStream) {
         var video = document.querySelector('video');
