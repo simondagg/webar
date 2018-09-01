f1b8d2a18be37cfb9c11a9898275e6e6f3a2ce11
simondagg <31724076+simondagg@users.noreply.github.com>
2018-09-02 01:38:03 +0800

    Add files via upload

--- pk/js/fbInit.js
+++ pk/js/fbInit.js
@@ -5,49 +5,23 @@ var config = {
     projectId: "fir-1-2b746",
     storageBucket: "fir-1-2b746.appspot.com",
     messagingSenderId: "772658594851"
-};
-firebase.initializeApp(config);
-
-var userID;
-
-//-------------------接收Android資料--------------------
-function currentUser(msg) {
-    // 取得使用者ID
-    userID = msg;
-
-}
-
-//var id;
-//id = randomWord(false,10);
-function randomWord(randomFlag, min, max) {
-    var str = "",
-        range = min,
-        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-    // 随机产生
-    if (randomFlag) {
-        range = Math.round(Math.random() * (max - min)) + min;
-    }
-    for (var i = 0; i < range; i++) {
-        pos = Math.round(Math.random() * (arr.length - 1));
-        str += arr[pos];
-    }
-    return str;
-}
-
-var updataObj = (dataArray) => {
-    dataArray = [];
-    var i = 0;
-    var query = firebase.database().ref("Object").orderByKey();
-    query.once("value")
-        .then(function (snapshot) {
-            snapshot.forEach(function (childSnapshot) {
-                var key = childSnapshot.key;
-                var childData = childSnapshot.val();
-                dataArray.push(childData);
-                dataArray[i] = Object.assign(dataArray[i], { objid: key });
-                i = i + 1;
-            });
-        });
-    return dataArray;
-};
+  };
+  firebase.initializeApp(config);
+  
+  //var id;
+  //id = randomWord(false,10);
+          function randomWord(randomFlag, min, max){
+              var str = "",
+                  range = min,
+                  arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+          
+              // 随机产生
+              if(randomFlag){
+                  range = Math.round(Math.random() * (max-min)) + min;
+              }
+              for(var i=0; i<range; i++){
+                  pos = Math.round(Math.random() * (arr.length-1));
+                  str += arr[pos];
+              }
+              return str;
+          }
---
+++ pk/js/han2.js
@@ -0,0 +1,33 @@
+var front = false;
+
+var constraints = { video: { facingMode: (front ? "user" : "environment"), width: 1920, height: 1080 } };
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (mediaStream) {
+        var video = document.querySelector('video');
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = function (e) {
+            video.play();
+        };
+    })
+    .catch(function (err) { console.log(err.name + ": " + err.message); });
+function getgps() {
+
+    function error() {
+        alert("Unable to retrieve your location");
+    }
+
+
+    function success(position) {
+        var latitude = position.coords.latitude * 100000;
+        var longitude = position.coords.longitude * 100000;
+        //ggggggggggggggggggggggggggggggggggggggggggggggg
+        var camera= document.getElementById('camera');
+        
+        camera.object3D.position.set(latitude,0,longitude);
+        
+        document.getElementById('miku').object3D.position.set(camera.object3D.position.x+5,0, camera.object3D.position.z+5);
+
+    }
+    navigator.geolocation.getCurrentPosition(success, error)
+}
+setInterval(getgps, 3000);
