af0d81f0a07678b40732b8f1778bfb24ebe9be21
simondagg <31724076+simondagg@users.noreply.github.com>
2018-09-02 01:43:38 +0800

    Add files via upload

--- js/han.js
+++ js/han.js
@@ -1,21 +1,12 @@
-function getgps() {
+var front = false;
 
-    function error() {
-        alert("Unable to retrieve your location");
-    }
-
-
-    function success(position) {
-        var latitude = position.coords.latitude * 100000;
-        var longitude = position.coords.longitude * 100000;
-        //ggggggggggggggggggggggggggggggggggggggggggggggg
-        var camera= document.getElementById('camera');
-        
-        camera.object3D.position.set(latitude,0,longitude);
-        
-        document.getElementById('miku').object3D.position.set(camera.object3D.position.x+3,0, camera.object3D.position.z+3);
-
-    }
-    navigator.geolocation.getCurrentPosition(success, error)
-}
-setInterval(getgps, 3000);
+var constraints = { video: { facingMode: (front ? "user" : "environment"), width: 1280, height: 720 } };
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (mediaStream) {
+        var video = document.querySelector('video');
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = function (e) {
+            video.play();
+        };
+    })
+    .catch(function (err) { console.log(err.name + ": " + err.message); });
--- js/putObjForm.js
+++ js/putObjForm.js
@@ -6,14 +6,12 @@
 //ob_type id="ob_type"
 //carousel-inner id="objImg1" "objImg2" "objImg3"
 //img src
-
-
 let src=[];
 src[0]='img/bulbasaur.jpg';
 src[1]='img/geodude.jpg';
 src[2]='img/poliwhirl.jpg';
 
-//let user="FDZQcGP1RjbfLACovlORVyYRXMJ2";//////////要記得刪
+let user="FDZQcGP1RjbfLACovlORVyYRXMJ2";//////////要記得刪
 
 let putObjForm='<div class="ui message" id="sendForm"> <i class="close icon" onclick="closeIcon()"></i> <div class="header" style="font-family:"微軟正黑體"; font-size:150%"> 你希望留下甚麼資訊?</div> <div class="object"> <form name="reg"></form> 標題： <input id="ob_title" name="ob_title" type="text" placeholder="請輸入10字以內" required maxlength="10" /><br/> 內容： <input id="ob_content" name="ob_title" type="text" placeholder="請輸入30字以內" /><br/> 類型： <select id="ob_type" name="ob_type" rel="--"> <option value="交友">交友</option> <optgroup> <option>交友</option> <option>揪團</option> <option>其他</option> </optgroup> </select><br/> <br/> <div id="select"> <div class="container-fluid"> <div class="row"> <div class="col-md-12"> <div class="carousel slide" id="carousel-19915" data-interval="0"> <div class="carousel-inner"> <div id="objImg1" class="carousel-item active"> <img class="d-block w-100" alt="Carousel Bootstrap First" src='+src[0]+' > </div> <div id="objImg2" class="carousel-item"> <img class="d-block w-100" alt="Carousel Bootstrap Second" src='+src[1]+' > </div> <div id="objImg3" class="carousel-item"> <img class="d-block w-100" alt="Carousel Bootstrap Third" src='+src[2]+' > </div> </div> <a class="carousel-control-prev" href="#carousel-19915" data-slide="prev"> <span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#carousel-19915" data-slide="next"> <span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span> </a> </div> </div> </div> </div> </div> <br/> <button id="OK" onclick="okButton()">OK</button> <button id="cancel" onclick="closeIcon()">取消</button> <div> </div> </form> </div></div>';
 document.getElementById("putObjForm").innerHTML=putObjForm;
@@ -51,8 +49,8 @@ let okButton = function () {
     }
 
 
-    let oid = randomWord(false,16);
-    
+    //let oid = randomWord(false,16);
+    let oid ="GCTELQTFMSWYCYZN";
     firebase.database().ref('Object/'+oid).set({
         module: obj_id,
         Latitude: lat,
@@ -61,14 +59,14 @@ let okButton = function () {
         date: firebase.database.ServerValue.TIMESTAMP,
         title: ob_title,
         type: ob_type,
-        user: "FDZQcGP1RjbfLACovlORVyYRXMJ2",
+        user: user,
         x :XY[0],
         y: XY[1]
 
       });
     console.log("創建成功");
     noneById("sendForm");
-    init();
+
 };
 
 let closeIcon=function(){
